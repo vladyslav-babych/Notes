0f2998bdc1ba35200813ddfe5760fecd9cb18b45
Vladyslav Babych <88340725+vladyslav-babych@users.noreply.github.com>
2022-08-03 14:23:58 +0300

    Refactored Parcelable implementation

--- app/src/main/java/com/otaman/notes/model/Note.kt
+++ app/src/main/java/com/otaman/notes/model/Note.kt
@@ -1,13 +1,13 @@
 package com.otaman.notes.model
 
-import android.os.Parcel
 import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 import java.util.*
 
-@Entity
+@Entity @Parcelize
 data class Note(
     @PrimaryKey
     var id: String = UUID.randomUUID().toString(),
@@ -17,30 +17,4 @@ data class Note(
 
     @ColumnInfo (name = "content")
     val content: String
-): Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(id)
-        parcel.writeString(title)
-        parcel.writeString(content)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Note> {
-        override fun createFromParcel(parcel: Parcel): Note {
-            return Note(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Note?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+): Parcelable
